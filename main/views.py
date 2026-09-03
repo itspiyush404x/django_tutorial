4381bb51a210011777bee2e97964f4832aa9f061
Piyush Saini <piyush9680saini@gmail.com>
2026-09-03 14:48:07 +0530

    made register and login page

--- main/views.py
+++ main/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse,JsonResponse
 from django.conf import settings
 from django.shortcuts import render
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 import json
 import os
 
 # Create your views here.
 def home(request):
+    return HttpResponse("<h1>This is Home Page<h1/>")
+
     if request.method == "GET":
         json_path = os.path.join(settings.MEDIA_ROOT, "data", "products.json")
         try:
@@ -31,4 +35,46 @@ def home(request):
         HttpResponse("Method not allowed", status=405)
 
 
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
+
+        if password != password2:
+            messages.info(request, "Passwords are not matching")
+            return redirect("register")
+        else: # password == password2
+            if User.objects.filter(username=username).exists():
+                messages.info(request, "Username already used")
+                return redirect("register")
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, "Email already exist")
+                return redirect("register")
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect("login")
+    else:
+        return render(request, "register.html")
+    
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        print(username, password)
+
+        user = auth.authenticate(username=username, password=password)
+
+        print(user)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("home")
+        else:
+            messages.info(request, "Incorrect Credentials")
+            return redirect("login")
+    else:
+        return render(request, "login.html")
     
